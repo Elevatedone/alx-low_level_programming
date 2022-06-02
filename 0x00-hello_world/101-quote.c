e6d8e18e369c2d95c542e2ce5c7ace0cf219c9a4
omowunmi oluwatosin <michealtosin@gmail.com>
2022-06-02 05:06:28 -0700

    re-modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 
 /**
  *  * main - Entry point
- *   * Return: Always 1 (Success)
- *    */
+ *   * Return: Always 1 (Success)   
+ */
 int main(void)
 {
 write(STDOUT_FILENO, "and that piece of art is useful
