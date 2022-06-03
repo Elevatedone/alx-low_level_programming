eab74ac7e43f40dd1f1f4738a83cc6f6ebacaf78
omowunmi oluwatosin <michealtosin@gmail.com>
2022-06-03 06:21:18 -0700

    positive-negative

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void)
+{
+	char lowercaseAlphabet;
+
+	for (lowercaseAlphabet = 'a'; lowercaseAlphabet <= 'z'; lowercaseAlphabet++)
+	putchar(lowercaseAlphabet);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *    
+ */
+
+int main(void)
+{
+	char lowercaseAlphabet;
+		
+
+	for (lowercaseAlphabet = 'a'; lowercaseAlphabet <= 'z'; lowercaseAlphabet++)
+
+		putchar(lowercaseAlphabet);
+	
+	for (lowercaseAlphabet = 'A'; lowercaseAlphabet <= 'Z'; lowercaseAlphabet++)
+
+		putchar(lowercaseAlphabet);
+
+	putchar('\n');
+	return (0);
+}
