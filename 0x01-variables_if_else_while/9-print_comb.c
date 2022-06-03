40d5068c36c54004c7c30ed52b82329f6a9b2d6c
OLUWATOSIN OMOWUNMI <97523126+Elevatedone@users.noreply.github.com>
2022-06-03 14:42:45 +0100

    combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,9 +18,9 @@ int main(void)
 	putchar(d);
 	if (d != '9')
 	{
-																putchar(',');
-																putchar(' ');
-																}
+		putchar(',');
+		putchar(' ');
+	}
 		}
 	putchar('\n');
 
