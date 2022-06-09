7cde14b8e8c700588fad18c9e9c7a1fa3c63403e
omowunmi oluwatosin <michealtosin@gmail.com>
2022-06-09 09:14:38 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+/*
+ * File: MAIN.h
+ * Auth: omowunmi oluwatosin
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x02-functions_nested_loops directory.
+ */
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i); 
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif
